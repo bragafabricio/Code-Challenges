09b55fec803e3a2491d330792b4060a1bb417c34
Fabricio Braga <fabriciobrg@gmail.com>
2022-06-30 18:13:28 -0300

    Create Leetcode_2011_Final_Value_of_Variable_After_Performing_Operations.java

---
+++ Leetcode_2011_Final_Value_of_Variable_After_Performing_Operations.java
@@ -0,0 +1,15 @@
+// 2011. Final Value of Variable After Performing Operations
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String operation : operations){
+            if (operation.contains("+")){
+                x += 1;
+            } else {
+                x -= 1;
+            }
+        }
+        return x;
+    }
+}
